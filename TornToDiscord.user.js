4c6239dd8efa213d108b52d7a073ca4904d0f7ac
shane.a.vangorder <shane.a.vangorder@gmail.com>
2017-08-13 22:47:55 -0400

    first commit

---
+++ TornToDiscord.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         TornToDiscord
+// @namespace    http://sigmadevops.com
+// @version      0.1
+// @description  Pushes a message from Torn chat to Discord
+// @author       saeed [1826888]
+// @match        *://www.torn.com/*
+// @downloadURL  http://sigmadevops.com/js/nnbtrainer.user.js
+// @updateURL    http://sigmadevops.com/js/nnbtrainer.user.js
+// ==/UserScript==
