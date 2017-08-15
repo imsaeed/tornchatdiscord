c6ac4324fe78c3614e3388fc2b1a4e26f8675c7a
Shane Van Gorder <imsaeed@users.noreply.github.com>
2017-08-15 17:19:33 -0500

    Update TornToDiscord.user.js

--- TornToDiscord.user.js
+++ TornToDiscord.user.js
@@ -1,21 +1,27 @@
 // ==UserScript==
-// @name         TornToDiscord
+// @name         TornToDiscord-Trade
 // @namespace    http://sigmadevops.com
 // @version      0.1
-// @description  Pushes a message from Torn chat to Discord
+// @description  Pushes trade chat from Torn to Discord
 // @author       saeed [1826888]
 // @match        *://www.torn.com/*
-// @downloadURL  https://github.com/imsaeed/tornchatdiscord/blob/master/TornToDiscord.user.js
-// @updateURL    https://github.com/imsaeed/tornchatdiscord/blob/master/TornToDiscord.user.js
+// @grant        GM_setValue
+// @grant        GM_getValue
 // ==/UserScript==
+
 //Get Chat Secret
 var secretRegex = document.body.textContent.match (/chat.init.(.*)chatRoot'/);
 var chatSecret = secretRegex[0].split(",");
-var chatSecret = chatSecret[3].replace(" ","").slice(1, -1);
+var Secret = chatSecret[3].replace(" ","").slice(1, -1);
+var uid = chatSecret[1].replace(" ","").slice(1,-1);
 
+if(GM_getValue("ttdIsLoaded") != "1") {
+//set "is open"
+console.log("TTD Loaded");
+GM_setValue("ttdIsLoaded", '1');
 
 //Open the websocket for viewing
-var webSocket = new WebSocket('wss://ws.torn.com/chat/ws?uid=1826888&secret=b6457aae92cdf25f11d15cb2d82df06e22ebe4e9986c1d09195b85708f2bcfc7');
+var webSocket = new WebSocket('wss://ws.torn.com/chat/ws?uid=' + uid + '&secret=' + Secret);
     webSocket.onopen = function (e) {
         console.log('websocket opened');
     };
@@ -25,15 +31,22 @@ webSocket.onerror = function(e) {
     };
 webSocket.onmessage = function(msg){
   var msgParse = JSON.parse(msg.data);
-  dataToSend = JSON.stringify({ "content": msgParse.data[0].messageText + " Sent by:" + msgParse.data[0].senderName});
+  dataToSend = JSON.stringify({ "content": msgParse.data[0].senderName + ': ' + msgParse.data[0].messageText});
 
 if (msgParse.data[0].messageText.indexOf('!f') !== -1) {
 
   $.ajax({
     type: 'POST',
-    async: false,
     url: 'https://discordapp.com/api/webhooks/346508040654028802/S8TcxcRa8Vo5LRhjpp5e609Jp6J6NzPRQ6VA9uGhU1_mG3YQPdAFxOFU32-XDN-12OST',
     data: dataToSend, // or JSON.stringify ({name: 'jonas'}),
     contentType: "application/json",
     dataType: 'json'
-});}}
+});
+}
+};
+    //unset when page close
+$(window).unload(function(){
+    console.log("TTD Unloaded");
+    GM_setValue("ttdIsLoaded", '0');
+});
+} else {console.log("TTD is already loaded! Not loading again.");}
